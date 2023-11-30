d735835ead80af614959a40903f17160b1c2e1d0
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-30 17:01:04 -0300

    working on

--- messageapp/Core/Authentication/Views/LoginView.swift
+++ messageapp/Core/Authentication/Views/LoginView.swift
@@ -13,7 +13,11 @@ struct LoginView: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+            }
+        }
     }
 }
 
