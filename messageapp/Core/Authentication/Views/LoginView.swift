f9fd788363e13d31f226feafc76e5c710190827c
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-30 17:00:31 -0300

    working on loginview

--- messageapp/Core/Authentication/Views/LoginView.swift
+++ messageapp/Core/Authentication/Views/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var passwrod = ""
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
