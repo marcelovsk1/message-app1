da5e40ec6d39a0e8142c539c7866bb3ad2e629db
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-30 16:59:22 -0300

    working on

---
+++ messageapp/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-30.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
