e257819a0665bd4267edf5adff65c5035e4ab3eb
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-30 16:55:15 -0300

    Initial Commit

---
+++ messageapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ messageapp/messageappApp.swift
@@ -0,0 +1,17 @@
+//
+//  messageappApp.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-30.
+//
+
+import SwiftUI
+
+@main
+struct messageappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
