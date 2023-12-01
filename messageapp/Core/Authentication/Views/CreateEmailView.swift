16785c963f6897d283b25c8681a2ca63cfd99b4a
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-01 16:12:11 -0300

    creating createemailview

---
+++ messageapp/Core/Authentication/Views/CreateEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateEmailView.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-12-01.
+//
+
+import SwiftUI
+
+struct CreateEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateEmailView()
+}
