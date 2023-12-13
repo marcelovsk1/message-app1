ed07696aaa139c03ff3ceff27e513068c3f97ce3
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-13 10:31:16 -0300

    creating createpasswordview

---
+++ messageapp/Core/Authentication/Views/CreatePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePasswordView.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-12-13.
+//
+
+import SwiftUI
+
+struct CreatePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePasswordView()
+}
