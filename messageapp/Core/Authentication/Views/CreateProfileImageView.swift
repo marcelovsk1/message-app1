d546781fceeadbb3169bcb406cbe4ee92b65f54b
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-13 10:35:15 -0300

    creating profileimageview

---
+++ messageapp/Core/Authentication/Views/CreateProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateProfileImageView.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-12-13.
+//
+
+import SwiftUI
+
+struct CreateProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateProfileImageView()
+}
