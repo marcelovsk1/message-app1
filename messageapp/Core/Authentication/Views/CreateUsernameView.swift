f0a442f6efa14188a1d32e430ad586a1e304e5fa
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-13 10:02:39 -0300

    creating usernameview

---
+++ messageapp/Core/Authentication/Views/CreateUsernameView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateUsernameView.swift
+//  messageapp
+//
+//  Created by Marcelo Amaral Alves on 2023-12-13.
+//
+
+import SwiftUI
+
+struct CreateUsernameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateUsernameView()
+}
