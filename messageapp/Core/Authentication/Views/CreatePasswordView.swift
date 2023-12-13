2635a3778b14a6fb8d7402e7e0b5d88e7b42d275
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-13 10:33:44 -0300

    createpasswordview is done

--- messageapp/Core/Authentication/Views/CreatePasswordView.swift
+++ messageapp/Core/Authentication/Views/CreatePasswordView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct CreatePasswordView: View {
+    @State private var password = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            Text("Create your password")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            Text("You'll use this password to sign in to your account.")
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+            
+            SecureField("Password", text: $password)
+                .font(.subheadline)
+                .padding(12)
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .padding(.horizontal, 24)
+                .padding(.top, 5)
+            
+            NavigationLink {
+                //
+            } label: {
+                Text("Next")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 360, height: 44)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+                    .padding(.vertical)
+            }
+            
+            Spacer()
+        }
     }
 }
+
 
 #Preview {
     CreatePasswordView()
