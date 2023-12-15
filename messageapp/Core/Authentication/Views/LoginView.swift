adb0adc77390d6a2306216231e2f4a027610cd35
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-15 10:28:53 -0300

    working on

--- messageapp/Core/Authentication/Views/LoginView.swift
+++ messageapp/Core/Authentication/Views/LoginView.swift
@@ -70,3 +70,16 @@ struct LoginView: View {
 #Preview {
     LoginView()
 }
+
+
+struct CustomButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .frame(width: 360, height: 44)
+            .background(Color(.systemBlue))
+            .cornerRadius(8)
+    }
+}
