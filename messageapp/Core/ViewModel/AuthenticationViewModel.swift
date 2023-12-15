1bb6f3b7389a9ab44c30c89878dcb47428454b91
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-15 10:03:32 -0300

    working on

--- messageapp/Core/ViewModel/AuthenticationViewModel.swift
+++ messageapp/Core/ViewModel/AuthenticationViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import PhotosUI
+import SwiftUI
+
+class AuthenticationViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var username = ""
+    @Published var password = ""
+    
+    @Published var profileImage: Image?
+    
+    private var uiImage: UIImage?
+    
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet {
+            
+        }
+    }
+    
+    @MainActor
+    func loadImage() async {
+        guard let item = selectedImage else { return }
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+    }
+}
